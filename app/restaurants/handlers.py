e4c71d6a66fd747e10ab1909a2d4a79e7a458705
Daniel Yovchev <daniel3d@ymail.com>
2017-02-22 01:14:00 +0000

    Finish Menu items CRUD

--- app/restaurants/handlers.py
+++ app/restaurants/handlers.py
@@ -18,7 +18,6 @@ def before_request():
 @restaurants.route('/')
 @restaurants.route('/restaurants')
 def index():
-	#show full list of restaurants.
 	restaurants = db.session.query(Restaurant).order_by(desc(Restaurant.id)).all()
 	return render_template('restaurants.html', restaurants=restaurants)
 
@@ -30,35 +29,9 @@ def menu(id):
 	restaurant = db.session.query(Restaurant).get(id)
 	items = db.session.query(MenuItem).filter_by(restaurant_id=restaurant.id).all()
 	for cource in menu_cources:
-		menu.append({'name': cource['name'], 
+		menu.append({'id': cource['id'], 'name': cource['name'], 
 			'items': [item for item in items if item.course == cource['id']]})
 	return render_template('restaurants/menu.html', menu=menu, restaurant=restaurant)
-
-
-@restaurants.route('/restaurants/<int:id>/edit', methods=['GET', 'POST'])
-def edit(id):
-	#show full list of restaurants.
-	restaurant = db.session.query(Restaurant).get(id)
-	old_name = restaurant.name
-	if request.method == 'POST':
-		restaurant.name = request.form['name']
-		restaurant.image = request.form['image']
-		restaurant.description = request.form['description']
-		db.session.commit()
-		flash('Restaurant %s was updated!' % old_name, 'alert-success')
-		return redirect(url_for('restaurants.index'))
-	else:
-		return render_template('restaurants/form.html', restaurant=restaurant)
-
-
-@restaurants.route('/restaurants/<int:id>/delete')
-def delete(id):
-	#show full list of restaurants.
-	restaurant = db.session.query(Restaurant).get(id)
-	flash('Restaurant %s was deleted!' % restaurant.name, 'alert-danger')
-	db.session.delete(restaurant)
-	db.session.commit()
-	return redirect(url_for('restaurants.index'))
 
 
 @restaurants.route('/restaurants/create', methods=['GET', 'POST'])
@@ -71,7 +44,75 @@ def create():
 		restaurant.description = request.form['description']
 		db.session.add(restaurant)
 		db.session.commit()
-		flash('Restaurant %s was created!' % restaurant.name, 'alert-success')
+		flash('Restaurant <b>%s</b> was created!' % restaurant.name, 'alert-success')
 		return redirect(url_for('restaurants.index'))
 	else:
 		return render_template('restaurants/form.html', restaurant=None)
+
+@restaurants.route('/restaurants/<int:id>/edit', methods=['GET', 'POST'])
+@requires_login
+def edit(id):
+	restaurant = db.session.query(Restaurant).get(id)
+	old_name = restaurant.name
+	if request.method == 'POST':
+		restaurant.name = request.form['name']
+		restaurant.image = request.form['image']
+		restaurant.description = request.form['description']
+		db.session.commit()
+		flash('Restaurant <b>%s</b> was updated!' % old_name, 'alert-success')
+		return redirect(url_for('restaurants.index'))
+	else:
+		return render_template('restaurants/form.html', restaurant=restaurant)
+
+
+@restaurants.route('/restaurants/<int:id>/delete', methods=['POST'])
+@requires_login
+def delete(id):
+	restaurant = db.session.query(Restaurant).get(id)
+	flash('Restaurant <b>%s</b> was deleted!' % restaurant.name, 'alert-warning')
+	db.session.delete(restaurant)
+	db.session.commit()
+	return redirect(url_for('restaurants.index'))
+
+
+@restaurants.route('/restaurants/<int:id>/menu/item/create', methods=['POST'])
+@requires_login
+def create_menu_item(id):
+	item = MenuItem()
+	item.name = request.form['name']
+	item.price = request.form['price']
+	item.course = request.form['course_id']
+	item.description = request.form['description']
+	item.restaurant_id = request.form['restaurant_id']
+	db.session.add(item)
+	db.session.commit()
+	flash('Menu item <b>%s</b> was created!' % item.name, 'alert-success')
+	return redirect(url_for('restaurants.menu', id=item.restaurant_id))
+
+
+@restaurants.route('/restaurants/menu/item/<int:id>/edit', methods=['POST'])
+@requires_login
+def edit_menu_item(id):
+	#show full list of restaurants.
+	item = db.session.query(MenuItem).get(id)
+	old_name = item.name
+	item.name = request.form['name']
+	item.price = request.form['price']
+	item.course = request.form['course_id']
+	item.description = request.form['description']
+	db.session.commit()
+	flash('Menu item <b>%s</b> was updated!' % old_name, 'alert-success')
+	return redirect(url_for('restaurants.menu', id=item.restaurant.id))
+
+
+@restaurants.route('/restaurants/menu/item/<int:id>/delete', methods=['POST'])
+@requires_login
+def delete_menu_item(id):
+	#show full list of restaurants.
+	item = db.session.query(MenuItem).get(id)
+	restaurant = item.restaurant
+	message = 'Menu item <b>%s</b> was deleted from restaurant <b>%s</b>!'
+	flash(message % (item.name, restaurant.name), 'alert-warning')
+	db.session.delete(item)
+	db.session.commit()
+	return redirect(url_for('restaurants.menu', id=restaurant.id))
